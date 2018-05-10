9c73a37e18bfa09854082655537579f563bd0c39
Dana Palazzo <dana@fretless.com>
2018-05-10 14:24:21 -0400

    Add query for link feed

---
+++ src/components/LinkList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import Link from './Link'
+
+class LinkList extends Component {
+  render() {
+    const linksToRender = [
+      {
+        id: '1',
+        description: 'Prisma turns your database into a GraphQL API 😎 😎',
+        url: 'https://www.prismagraphql.com',
+      },
+      {
+        id: '2',
+        description: 'The best GraphQL client',
+        url: 'https://www.apollographql.com/docs/react/',
+      },
+    ]
+
+    return (
+      <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+    )
+  }
+}
+
+// 1
+const FEED_QUERY = gql`
+  # 2
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+// 3
+export default graphql(FEED_QUERY, { name: 'feedQuery' }) (LinkList)
